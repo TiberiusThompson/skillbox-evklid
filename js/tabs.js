41ff89e0cb984789b5ebe75893c71d8cc0d9c331
TiberiusThompson <maks.100h@yandex.ru>
2021-09-25 17:23:23 +0300

    added swipper in "special offer". need to correct the tabs in the section "our-work"

---
+++ js/tabs.js
@@ -0,0 +1,14 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.our-work-item__btn').forEach(function(tabsBtn) {
+        tabsBtn.addEventListener('click', function(event) {
+            const path = event.currentTarget.dataset.path;
+            console.log('path')
+
+            document.querySelectorAll('.our-work-content').forEach(function(tabContent) {
+                tabContent.classList.remove('our-work-content-active')
+            })
+            document.querySelector('[data-target="S{path}"]').classList.add('our-work-content-active')
+        })
+    })
+}) 
