4f96d75aeb665dbbab837f95a8830fe8ac76cf9d
TiberiusThompson <maks.100h@yandex.ru>
2021-11-10 21:02:40 +0300

    fix of tabs

--- js/tabs.js
+++ js/tabs.js
@@ -19,32 +19,6 @@ window.addEventListener("DOMContentLoaded", function() {
             .querySelector(`[data-target="${path}"]`).classList.add("our-work-content-active")
         })
 
-        // document.querySelectorAll("#header-search").forEach(function(act) {
-        //     act.addEventListener('click', function(event1) {
-        //         console.log("event1")
-        //         event1.target.classList.toggle('header-search-form-active')
-        //     })
-        // })
-
-        // document.querySelector("#header-search").addEventListener('click', function() {
-        //     document.querySelector("#search-menu").classList.toggle("header-search-form-active")
-        // })
-
-        // document.querySelectorAll('.our-work-item__btn').forEach
-
-        // document
-        // .querySelectorAll('.our-work-item__btn')
-        // .forEach(function(BTNactive) {
-        //   BTNactive.classList.remove('our-work-item__btn_active')
-        // })
-
-        // document.
-        // .querySelector(`[data-target="${path}"]`).classList.add("our-work-item__btn_active")
     })
 
-    // document.querySelectorAll('.our-work-item__btn').forEach(function(act) {
-    //   act.addEventListener('click', function(click) {
-    //     click.target.classList.toggle('our-work-item__btn_active')
-    //   })
-    // })
 }) 
